0a294e9a30fa2fda0da6b415652dec3e62850f27
Jonath Jimmi <jonath.jimmi@gmail.com>
2025-06-29 22:29:50 +0530

    Add files via upload

---
+++ models/neural_network.py
@@ -0,0 +1,33 @@
+import torch.nn as nn
+
+class FeedForwardNN(nn.Module):
+    def __init__(self, input_size=561, hidden_layers=[128, 64], num_classes=6, dropout_rate=0.2):
+        super().__init__()
+        layers = []
+        prev_size = input_size
+        
+        for hidden_size in hidden_layers:
+            layers.extend([
+                nn.Linear(prev_size, hidden_size),
+                nn.ReLU(),
+                nn.Dropout(dropout_rate)
+            ])
+            prev_size = hidden_size
+        
+        layers.append(nn.Linear(prev_size, num_classes))
+        self.network = nn.Sequential(*layers)
+        self._initialize_weights()
+        
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+                nn.init.constant_(m.bias, 0)
+                
+    def forward(self, x):
+        return self.network(x)
+        
+    def get_model_size(self):
+        param_size = sum(p.nelement() * p.element_size() for p in self.parameters())
+        buffer_size = sum(b.nelement() * b.element_size() for b in self.buffers())
+        return (param_size + buffer_size) / 1024**2
