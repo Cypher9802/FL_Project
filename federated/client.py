c82ca065ac9c618dfb5425f449b1feaff8c3fb39
Jonath Jimmi <jonath.jimmi@gmail.com>
2025-07-01 09:18:52 +0530

    Update client.py @9:18 1st Jul

--- federated/client.py
+++ federated/client.py
@@ -4,19 +4,19 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader, TensorDataset
 from opacus import PrivacyEngine
 from opacus.utils.batch_memory_manager import BatchMemoryManager
+from config import Config
 
 class Client:
-    def __init__(self, cid, data, config):
-        self.id = cid
-        self.config = config
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    def __init__(self, client_id, data):
+        self.client_id = client_id
+        self.device = Config.DEVICE
         X_train, y_train = data['train']['X'], data['train']['y']
-        X_val,   y_val   = data['validate']['X'], data['validate']['y']
+        X_val, y_val = data['validate']['X'], data['validate']['y']
         self.train_loader = DataLoader(
-            TensorDataset(torch.tensor(X_train), torch.tensor(y_train)),
-            batch_size=self.config['federated']['batch_size'], shuffle=True, drop_last=True)
+            TensorDataset(torch.tensor(X_train, dtype=torch.float32), torch.tensor(y_train, dtype=torch.long)),
+            batch_size=Config.BATCH_SIZE, shuffle=True, drop_last=True)
         self.val_loader = DataLoader(
-            TensorDataset(torch.tensor(X_val), torch.tensor(y_val)),
+            TensorDataset(torch.tensor(X_val, dtype=torch.float32), torch.tensor(y_val, dtype=torch.long)),
             batch_size=len(y_val), shuffle=False)
         self.privacy_spent = 0.0
 
@@ -25,26 +25,42 @@ class Client:
         model.train()
         optimizer = torch.optim.SGD(
             model.parameters(),
-            lr=self.config['training']['learning_rate'],
-            momentum=self.config['training']['momentum'],
-            weight_decay=self.config['training']['weight_decay'])
-        if self.config['privacy']['epsilon'] > 0:
-            engine = PrivacyEngine()
-            model, optimizer, self.train_loader = engine.make_private_with_epsilon(
+            lr=Config.LR,
+            momentum=Config.MOMENTUM,
+            weight_decay=Config.WEIGHT_DECAY)
+        if Config.EPSILON > 0:
+            privacy_engine = PrivacyEngine()
+            model, optimizer, self.train_loader = privacy_engine.make_private_with_epsilon(
                 module=model,
                 optimizer=optimizer,
                 data_loader=self.train_loader,
-                epochs=self.config['federated']['local_epochs'],
-                target_epsilon=self.config['privacy']['epsilon'] / self.config['federated']['rounds'],
-                target_delta=self.config['privacy']['delta'],
-                max_grad_norm=self.config['privacy']['max_grad_norm'])
-        for _ in range(self.config['federated']['local_epochs']):
-            for X, y in self.train_loader:
-                X, y = X.to(self.device), y.to(self.device)
-                optimizer.zero_grad()
-                loss = F.cross_entropy(model(X), y)
-                loss.backward()
-                optimizer.step()
-        if self.config['privacy']['epsilon'] > 0:
-            self.privacy_spent = engine.get_epsilon(self.config['privacy']['delta'])
+                epochs=Config.LOCAL_EPOCHS,
+                target_epsilon=Config.EPSILON / Config.ROUNDS,
+                target_delta=Config.DELTA,
+                max_grad_norm=Config.MAX_GRAD_NORM)
+        criterion = torch.nn.CrossEntropyLoss()
+        for _ in range(Config.LOCAL_EPOCHS):
+            with BatchMemoryManager(
+                data_loader=self.train_loader,
+                max_physical_batch_size=Config.BATCH_SIZE,
+                optimizer=optimizer) as memory_safe_loader:
+                for X, y in memory_safe_loader:
+                    X, y = X.to(self.device), y.to(self.device)
+                    optimizer.zero_grad()
+                    loss = criterion(model(X), y)
+                    loss.backward()
+                    optimizer.step()
+        if Config.EPSILON > 0:
+            self.privacy_spent = privacy_engine.get_epsilon(Config.DELTA)
         return model.state_dict()
+
+    def evaluate(self, model):
+        model.eval()
+        correct, total = 0, 0
+        with torch.no_grad():
+            for X, y in self.val_loader:
+                X, y = X.to(self.device), y.to(self.device)
+                preds = model(X).argmax(dim=1)
+                correct += (preds == y).sum().item()
+                total += y.size(0)
+        return correct / total
